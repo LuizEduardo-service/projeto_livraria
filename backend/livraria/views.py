7819e5691577c7167ca50dd9a64059d82fec2c5b
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-20 15:57:45 -0300

    feat: updating method get and post category

--- backend/livraria/views.py
+++ backend/livraria/views.py
@@ -1,12 +1,19 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from .models import Categoria
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
 class CategoriaView(View):
 
     def get(self, request):
-        data = list(Categoria.objects.all())
-        formatted_data = json.dumps(data, ensure_ascii=False)
-        return HttpResponse(formatted_data, content_type='application/json')
+        data = list(Categoria.objects.values())
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+    def post(self, request):
+        json_data = json.loads(request.body)
+        nova_categoria: Categoria = Categoria.objects.create(**json_data)
+        data = {"id": nova_categoria.id, 'descricao': nova_categoria.descricao}
+        return JsonResponse(data=data)
