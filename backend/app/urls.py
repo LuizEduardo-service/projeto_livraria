5dc3fe816a028dd7519e467264749ed7b362e1bd
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-20 17:52:06 -0300

    feat: include rest-framework and implement APIView

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
+
 from livraria import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('categorias/', view=views.CategoriaView.as_view(), name='categorias')
+    path('categorias/', view=views.CategoriaView.as_view(), name='categorias'),
+    path('categorias-apiview/', view=views.CategoriaList.as_view(), name='categorias-api')
 ]
--- backend/livraria/views.py
+++ backend/livraria/views.py
@@ -1,9 +1,16 @@
 import json
-from django.http import HttpResponse, JsonResponse
-from django.views import View
-from .models import Categoria
+from typing import List
+
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import Categoria
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CategoriaView(View):
@@ -17,3 +24,25 @@ class CategoriaView(View):
         nova_categoria: Categoria = Categoria.objects.create(**json_data)
         data = {"id": nova_categoria.id, 'descricao': nova_categoria.descricao}
         return JsonResponse(data=data)
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ('id', 'descricao', )
+
+
+class CategoriaList(APIView):
+
+    def get(self, request):
+        categorias: List[Categoria] = Categoria.objects.all()
+        serializers = CategoriaSerializer(categorias, many=True)
+        return Response(serializers.data)
+
+    def post(self, request):
+        serializer = CategoriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
