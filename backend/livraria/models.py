4a2aea3bf57abefe4006a7ebe40f9db1274d2de6
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-20 14:15:36 -0300

    feat: create models categoria and editora

--- backend/livraria/models.py
+++ backend/livraria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Categoria(BaseModel):
+    descricao = models.CharField(max_length=255)
+
+class Editora(BaseModel):
+    nome = models.CharField(max_length=255)
+    site = models.URLField()
