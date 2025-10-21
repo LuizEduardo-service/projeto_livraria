b4bec9a695e1cef950d624538bf01ce2bfa31fff
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-21 12:32:03 -0300

    feat: create views and serializer of livros and include in file urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,12 +4,14 @@ from django.urls import path, include
 
 from livraria.views.categoria import CategoriaViewSet
 from livraria.views.editora import EditoraViewSet
+from livraria.views.livro import LivrosViewSet
 
 from rest_framework import routers
 
 routes = routers.DefaultRouter()
 routes.register(r'categorias', CategoriaViewSet)
 routes.register(r'editoras', EditoraViewSet)
+routes.register(r'livros', LivrosViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ backend/livraria/serializers/livro.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+
+from livraria.models import Livros
+
+
+class LivrosSerializer(ModelSerializer):
+    
+    class Meta:
+        model = Livros
+        fields = '__all__'
+
+
+
+class LivrosDetailSerializer(ModelSerializer):
+
+    class Meta:
+        model = Livros
+        fields = '__all__'
+        depth = 1
