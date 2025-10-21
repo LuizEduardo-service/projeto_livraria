80ca6dfa29d3796c8e90864ff3132a31ea815f00
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-21 12:05:41 -0300

    feat: create views and serializer of editora and include in file urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from livraria.views.categoria import CategoriaViewSet
+from livraria.views.editora import EditoraViewSet
 
 from rest_framework import routers
 
 routes = routers.DefaultRouter()
 routes.register(r'categorias', CategoriaViewSet)
+routes.register(r'editoras', EditoraViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
