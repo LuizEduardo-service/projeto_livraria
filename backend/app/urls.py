d3cc2aeb27acd3e32a22c00fbf4e877c3446cdc9
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-23 10:56:37 -0300

    feat: include urls for jwt

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -8,6 +8,11 @@ from livraria.views.livro import LivrosViewSet
 from livraria.views.autor import AutorViewSet
 from livraria.views.compra import CompraViewSet
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from rest_framework import routers
 
 routes = routers.DefaultRouter()
@@ -17,8 +22,11 @@ routes.register(r'livros', LivrosViewSet)
 routes.register(r'autores', AutorViewSet)
 routes.register(r'compras', CompraViewSet)
 
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('', include(routes.urls))
+    path('', include(routes.urls)), 
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
