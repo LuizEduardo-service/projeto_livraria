057176d2477ce5bd7395a4b2c453ac0a4630ee7d
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-23 11:23:31 -0300

    feat: include swagger

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -13,6 +13,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework import routers
 
 routes = routers.DefaultRouter()
@@ -23,9 +24,13 @@ routes.register(r'autores', AutorViewSet)
 routes.register(r'compras', CompraViewSet)
 
 
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('', include(routes.urls)), 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
