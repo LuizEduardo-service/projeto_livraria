f9f5db69d2367791f5bb5e640a11e1e348729995
LuizEduardo-service <luizeduardo.service@outlook.com>
2025-10-30 10:29:32 -0300

    feat: include module redis cache

--- backend/livraria/views/compra.py
+++ backend/livraria/views/compra.py
@@ -1,4 +1,7 @@
+import time
 from rest_framework.viewsets import ModelViewSet
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from livraria.models import Compra
 from livraria.serializers.compra import (
@@ -16,3 +19,13 @@ class CompraViewSet(ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return CompraSerializer
         return CriarEditarCompraSerializer
+    
+    def get_queryset(self):
+        print("buscando da base de dados!")
+        time.sleep(2)
+        return super().get_queryset()
+
+    @method_decorator(cache_page(60 * 2, key_prefix='compra_list'))
+    def list(self, request, *args, **kwargs):
+        print("buscando do cache-redis!")
+        return super().list(request, *args, **kwargs)
